2da65674be1a40e94ebe468f9de7c3ef8446f079
VeYand <ValerikLerik17@gmail.com>
2024-11-09 21:32:25 +0300

    Настроил роутинг

--- backend/src/Controller/PageController.php
+++ backend/src/Controller/PageController.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PageController extends AbstractController
 {
-    public function mainPage(Request $request): Response
-    {
-        return $this->render('base.html.twig');
-    }
+	public function mainPage(): Response
+	{
+		return $this->render('base.html.twig');
+	}
 }
