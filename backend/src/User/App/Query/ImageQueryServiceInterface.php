36fd78ef2268c5621fb25a9492058675a85d3213
VeYand <ValerikLerik17@gmail.com>
2024-11-09 20:50:08 +0300

    Добавил получение img url

---
+++ backend/src/User/App/Query/ImageQueryServiceInterface.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace App\User\App\Query;
+
+interface ImageQueryServiceInterface
+{
+	public function getImageUrl(string $imagePath): string;
+}
---
+++ backend/src/User/Infrastructure/Query/ImageQueryService.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\User\Infrastructure\Query;
+
+use App\User\App\Query\ImageQueryServiceInterface;
+
+class ImageQueryService implements ImageQueryServiceInterface
+{
+	public function getImageUrl(string $imagePath): string
+	{
+		return $imagePath;
+	}
+}
