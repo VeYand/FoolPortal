2fa03fc2a50611fcea8e597e91984fe4ce03f18c
VeYand <ValerikLerik17@gmail.com>
2024-11-11 02:15:21 +0300

    Реализовал UserApi

--- backend/src/User/Api/UserApi.php
+++ backend/src/User/Api/UserApi.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 namespace App\User\Api;
 
 use App\User\App\Query\Data\UserData;
+use App\User\App\Query\GroupQueryServiceInterface;
 use App\User\App\Query\UserQueryServiceInterface;
+use App\User\App\Service\GroupMemberService;
+use App\User\App\Service\GroupService;
+use App\User\App\Service\UserService;
+use App\User\Domain\Service\Input\CreateUserInput;
+use App\User\Domain\Service\Input\UpdateUserInput;
 
 readonly class UserApi implements UserApiInterface
 {
 	public function __construct(
-		private UserQueryServiceInterface $userQueryService,
+		private UserQueryServiceInterface  $userQueryService,
+		private GroupQueryServiceInterface $groupQueryService,
+		private UserService                $userService,
+		private GroupService               $groupService,
+		private GroupMemberService         $groupMemberService,
 	)
 	{
 	}
@@ -28,5 +38,85 @@ readonly class UserApi implements UserApiInterface
 	public function getUserHashedPassword(string $userId): string
 	{
 		return $this->userQueryService->getUserHashedPassword($userId);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function listAllUsers(): array
+	{
+		return $this->userQueryService->listAllUsers();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function listAllGroups(): array
+	{
+		return $this->groupQueryService->listAllGroups();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function createUser(CreateUserInput $input): void
+	{
+		$this->userService->create($input);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function updateUser(UpdateUserInput $input): void
+	{
+		$this->userService->update($input);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function deleteUser(string $userId): void
+	{
+		$this->userService->delete($userId);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function createGroup(string $groupName): void
+	{
+		$this->groupService->create($groupName);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function updateGroup(string $groupId, string $groupName): void
+	{
+		$this->groupService->update($groupId, $groupName);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function deleteGroup(string $groupId): void
+	{
+		$this->groupService->delete($groupId);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function addUserToGroup(string $groupId, string $userId): void
+	{
+		$this->groupMemberService->addUserToGroup($groupId, $userId);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function removeUserFromGroup(string $groupId, string $userId): void
+	{
+		$this->groupMemberService->removeUserFromGroup($groupId, $userId);
 	}
 }
--- backend/src/User/Api/UserApiInterface.php
+++ backend/src/User/Api/UserApiInterface.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 namespace App\User\Api;
 
 use App\Common\Exception\AppException;
+use App\User\App\Query\Data\DetailedUserData;
+use App\User\App\Query\Data\GroupData;
 use App\User\App\Query\Data\UserData;
+use App\User\Domain\Service\Input\CreateUserInput;
+use App\User\Domain\Service\Input\UpdateUserInput;
 
 interface UserApiInterface
 {
@@ -17,4 +21,54 @@ interface UserApiInterface
 	 * @throws AppException
 	 */
 	public function getUserHashedPassword(string $userId): string;
+
+	/**
+	 * @return DetailedUserData[]
+	 */
+	public function listAllUsers(): array;
+
+	/**
+	 * @return GroupData[]
+	 */
+	public function listAllGroups(): array;
+
+	/**
+	 * @throws AppException
+	 */
+	public function createUser(CreateUserInput $input): void;
+
+	/**
+	 * @throws AppException
+	 */
+	public function updateUser(UpdateUserInput $input): void;
+
+	/**
+	 * @throws AppException
+	 */
+	public function deleteUser(string $userId): void;
+
+	/**
+	 * @throws AppException
+	 */
+	public function createGroup(string $groupName): void;
+
+	/**
+	 * @throws AppException
+	 */
+	public function updateGroup(string $groupId, string $groupName): void;
+
+	/**
+	 * @throws AppException
+	 */
+	public function deleteGroup(string $groupId): void;
+
+	/**
+	 * @throws AppException
+	 */
+	public function addUserToGroup(string $groupId, string $userId): void;
+
+	/**
+	 * @throws AppException
+	 */
+	public function removeUserFromGroup(string $groupId, string $userId): void;
 }
