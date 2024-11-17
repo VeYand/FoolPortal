0aaa8488538941d8f166178a615b5db08481caa4
VeYand <ValerikLerik17@gmail.com>
2024-11-17 20:37:40 +0300

    Поправил форматирование

--- backend/src/Subject/App/Adapter/Data/UserData.php
+++ backend/src/Subject/App/Adapter/Data/UserData.php
@@ -6,8 +6,8 @@ namespace App\Subject\App\Adapter\Data;
 readonly class UserData
 {
 	public function __construct(
-		public string $userId,
-		public UserRole $role
+		public string   $userId,
+		public UserRole $role,
 	)
 	{
 	}
