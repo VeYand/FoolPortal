3c2d18a5d615ea517edae93f7692bf3936669808
Snickers <Funbasskek@gmail.com>
2024-11-18 23:26:25 +0300

    Добавил тесты на создание пользователя, поправил ошибку с типизацией

--- backend/tests/Unit/User/Domain/Service/UserServiceTest.php
+++ backend/tests/Unit/User/Domain/Service/UserServiceTest.php
@@ -6,12 +6,15 @@ namespace App\Tests\Unit\User\Domain\Service;
 use App\Common\Uuid\UuidProvider;
 use App\Common\Uuid\UuidProviderInterface;
 use App\Tests\Unit\Common\MockEventPublisher;
-use App\Tests\Unit\User\Domain\Infrastructure\GroupMemberInMemoryRepository;
-use App\Tests\Unit\User\Domain\Infrastructure\MockImageUploader;
-use App\Tests\Unit\User\Domain\Infrastructure\UserInMemoryRepository;
+use App\Tests\Unit\User\Infrastructure\GroupMemberInMemoryRepository;
+use App\Tests\Unit\User\Infrastructure\MockImageUploader;
+use App\Tests\Unit\User\Infrastructure\UserInMemoryRepository;
+use App\User\Domain\Exception\DomainException;
+use App\User\Domain\Model\UserRole;
 use App\User\Domain\Repository\GroupMemberRepositoryInterface;
 use App\User\Domain\Repository\UserRepositoryInterface;
 use App\User\Domain\Service\ImageUploaderInterface;
+use App\User\Domain\Service\Input\CreateUserInput;
 use App\User\Domain\Service\PasswordHasherInterface;
 use App\User\Domain\Service\UserService;
 use App\User\Infrastructure\Hasher\PasswordHasher;
@@ -44,13 +47,45 @@ class UserServiceTest extends TestCase
 		);
 	}
 
-	public function testCase(): void
+	/**
+	 * @throws DomainException
+	 */
+	public function testCreateUser(): void
 	{
-		// подготовка данных
+		$input = new CreateUserInput(
+			'firstName',
+			'lastName',
+			null,
+			UserRole::ADMIN,
+			null,
+			'email@gmail.com',
+			'password',
+		);
 
-		// действие
+		$this->userService->create($input);
 
-		// проверка результата
-		$this->assertTrue(true);
+		$this->assertCount(1, $this->userRepository->findAll());
+	}
+
+	/**
+	 * @throws DomainException
+	 */
+	public function testCreateUserEmailIsAlreadyTaken(): void
+	{
+		$input = new CreateUserInput(
+			'firstName',
+			'lastName',
+			null,
+			UserRole::ADMIN,
+			null,
+			'email@gmail.com',
+			'password',
+		);
+		$this->userService->create($input);
+
+		$this->expectException(DomainException::class);
+		$this->expectExceptionCode(DomainException::EMAIL_IS_NOT_UNIQUE);
+
+		$this->userService->create($input);
 	}
 }
