b7c96526e0b18ab88d6054b93425dda661637cdf
VeYand <ValerikLerik17@gmail.com>
2024-11-22 19:46:58 +0300

    Настроил билдинг фронта, добавил использование апишки фронтом, добавил зависимости для фронта

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    OpenAPI\Server\OpenAPIServerBundle::class => ['all' => true],
 ];
