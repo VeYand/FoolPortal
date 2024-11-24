776121c42a30b525bfc0ad7b126290941c4898e6
VeYand <ValerikLerik17@gmail.com>
2024-11-24 15:24:47 +0300

    Отрефакторил middleware

--- frontend/src/shared/redux/middleware.ts
+++ frontend/src/shared/redux/middleware.ts
@@ -3,18 +3,25 @@ import {userEntitySlice} from 'entities/user'
 import {LoginRoute} from 'shared/routes'
 
 const authorizationMiddleware: Middleware = _ => next => action => {
-	if (
-		action.meta?.requestStatus === 'rejected'
-		&& action.error?.message
-		&& getStatusFromErrorMessage(action.error.message) === '401'
-	) {
-		next(userEntitySlice.actions.setInitialized(false))
-		if (window.location.pathname !== LoginRoute.path) {
-			window.location.replace(window.location.origin + LoginRoute.path)
-		}
+	if (isUnauthorizedError(action)) {
+		handleUnauthorizedAccess()
 	}
 
 	return next(action)
+}
+
+const isUnauthorizedError = (action: any): boolean => (
+	action.meta?.requestStatus === 'rejected'
+	&& action.error?.message
+	&& getStatusFromErrorMessage(action.error.message) === '401'
+)
+
+const handleUnauthorizedAccess = () => {
+	userEntitySlice.actions.setInitialized(false)
+
+	if (window.location.pathname !== LoginRoute.path) {
+		window.location.replace(window.location.origin + LoginRoute.path)
+	}
 }
 
 const getStatusFromErrorMessage = (message: string) => (message.split(' ').pop() as string)
