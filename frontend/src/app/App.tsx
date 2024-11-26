4d41559e9eab6a1e7d2494fecc9a9aa31c597743
VeYand <ValerikLerik17@gmail.com>
2024-11-26 20:34:49 +0300

    Вынес фичи в виджеты

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,9 +1,9 @@
 import {ConfigProvider} from 'antd'
-import {Preloader} from 'features/preloader/Preloader'
 import {useEffect} from 'react'
 import {Route, Routes} from 'react-router-dom'
 import {useInitializeUser} from 'shared/libs/hooks'
 import {LoginRoute, UserPortalRoute, ProfileRoute} from 'shared/routes'
+import {Preloader} from 'widgets/preloader/Preloader'
 import {LoginPage, UserPortalPage, NotFoundPage, ProfilePage} from '../pages'
 import {globalTheme} from './globalTheme'
 
