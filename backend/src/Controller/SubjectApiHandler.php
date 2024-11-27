b5abe731ac237e59fa75cdf48f92f258720bb21a
VeYand <ValerikLerik17@gmail.com>
2024-11-27 19:22:12 +0300

    добавил api для работы с teacherSubject

--- backend/src/Controller/SubjectApiHandler.php
+++ backend/src/Controller/SubjectApiHandler.php
@@ -8,9 +8,13 @@ use App\Controller\Exception\ExceptionHandler;
 use App\Subject\Api\SubjectApiInterface;
 use OpenAPI\Server\Api\SubjectApiInterface as SubjectApiHandlerInterface;
 use OpenAPI\Server\Model\CreateSubjectRequest;
+use OpenAPI\Server\Model\CreateTeacherSubjectsRequest;
 use OpenAPI\Server\Model\DeleteSubjectRequest;
+use OpenAPI\Server\Model\DeleteTeacherSubjectsRequest;
+use OpenAPI\Server\Model\ListTeacherSubjectRequest;
 use OpenAPI\Server\Model\SubjectsList as ApiSubjectsList;
 use OpenAPI\Server\Model\UpdateSubjectRequest;
+use OpenAPI\Server\Model\UpdateTeacherSubjectsRequest;
 
 readonly class SubjectApiHandler implements SubjectApiHandlerInterface
 {
@@ -67,5 +71,37 @@ readonly class SubjectApiHandler implements SubjectApiHandlerInterface
 		{
 			$this->subjectApi->deleteSubject($deleteSubjectRequest->getSubjectId());
 		}, $responseCode, $responseHeaders);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function createTeacherSubjects(CreateTeacherSubjectsRequest $createTeacherSubjectsRequest, int &$responseCode, array &$responseHeaders): void
+	{
+		// TODO: Implement createTeacherSubjects() method.
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function listTeacherSubjects(ListTeacherSubjectRequest $listTeacherSubjectRequest, int &$responseCode, array &$responseHeaders): array|object|null
+	{
+		// TODO: Implement listTeacherSubjects() method.
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function updateTeacherSubjects(UpdateTeacherSubjectsRequest $updateTeacherSubjectsRequest, int &$responseCode, array &$responseHeaders): void
+	{
+		// TODO: Implement updateTeacherSubjects() method.
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function deleteTeacherSubjects(DeleteTeacherSubjectsRequest $deleteTeacherSubjectsRequest, int &$responseCode, array &$responseHeaders): void
+	{
+		// TODO: Implement deleteTeacherSubjects() method.
 	}
 }
